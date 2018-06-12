3d1527d4e76210b946ccc8d15804eb95de1106c7
Gerry Suwignyo <gsuwignyo@gmail.com>
2018-06-12 22:05:29 +0000

    functionality works, but no test

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -35,19 +35,41 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", {errors : []});
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  var newShort = generateRandomString();
+  var errors = []
+  if (!req.body.longURL) {
+    errors.push('URL is required')
+  }
+  if (errors.length > 0){
+    res.send(errors[1])
+    // res.render('urls/new', {errors: errors})
+  } else{
+    urlDatabase[newShort] = req.body.longURL;
+    urlDatabase;
+    res.redirect('/urls');
+    }
+
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString(){
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(longURL)
+  res.redirect(longURL);
+});
 
+function generateRandomString (){
+  var rand = '';
+  rand = Math.random().toString(36).substring(2,8);
+  return rand;
 }
